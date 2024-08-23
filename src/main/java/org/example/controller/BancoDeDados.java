2233ba51ea924919eb667b445ab78b8aeae9cc40
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-23 14:20:13 -0300

    UPDATE Item.java AND BancoDeDados.java

--- src/main/java/org/example/controller/BancoDeDados.java
+++ src/main/java/org/example/controller/BancoDeDados.java
@@ -1,4 +1,40 @@
 package org.example.controller;
 
+import org.example.model.Item;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 public class BancoDeDados {
+
+    public BancoDeDados() {
+
+    }
+
+    public void cadastrar(Item item){
+
+        String linha = "";
+
+        try{
+            OutputStream os = new FileOutputStream("produtos.txt", true);
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            linha = item.getNome()+","+item.getCodigo();
+            bw.write(linha);
+            bw.newLine();
+
+            bw.close();
+            osw.close();
+            os.close();
+
+            System.out.println(linha +" Arquivo cadastrado com sucesso!");
+        } catch (Exception e){
+            System.out.println("Erro ao cadastrar no banco de dados");
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
--- src/main/java/org/example/model/Item.java
+++ src/main/java/org/example/model/Item.java
@@ -1,4 +1,27 @@
 package org.example.model;
 
 public class Item {
+    private String nome;
+    private int codigo;
+
+    public Item(String nome, int codigo) {
+        this.setNome(nome);
+        this.setCodigo(codigo);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 }
